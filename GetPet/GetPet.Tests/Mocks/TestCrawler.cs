2bb7df56518f4e9f5c072accad6749ee43fbbb89
Liza Lechtman <lizalecht@gmail.com>
2021-05-04 22:36:53 +0300

    Fixed unitests. PLEASE DO NOT PUT THEM IN COMMENT AGAIN

--- GetPet/GetPet.Tests/Mocks/TestCrawler.cs
+++ GetPet/GetPet.Tests/Mocks/TestCrawler.cs
@@ -1,7 +1,8 @@
-﻿using GetPet.Crawler;
+﻿using GetPet.BusinessLogic;
+using GetPet.BusinessLogic.Handlers.Abstractions;
 using GetPet.Crawler.Crawlers;
-using GetPet.Crawler.Parsers;
 using GetPet.Crawler.Parsers.Abstractions;
+using PetAdoption.BusinessLogic.Repositories;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -9,22 +10,26 @@ using System.Text;
 
 namespace GetPet.Tests.Mocks
 {
-    //public class TestCrawler<T> : CrawlerBase<T> where T : IParser, new()
-    //{
-    //    protected override string url => throw new NotImplementedException();
+    public class TestCrawler<T> : CrawlerBase<T> where T : IParser, new()
+    {
+        public TestCrawler(IPetHandler petHandler, IPetRepository petRepository, IUnitOfWork unitOfWork) : base(petHandler, petRepository, unitOfWork)
+        {
+        }
 
-    //    public override void Load(string url)
-    //    {
-    //        if (!File.Exists(url))
-    //        {
-    //            throw new Exception("Cannot find file");
-    //        }
+        protected override string url => throw new NotImplementedException();
 
-    //        using (var file = File.OpenRead(url))
-    //        {
-    //            doc.Load(file);
-    //            parser.Document = doc;
-    //        }
-    //    }
-    //}
+        public override void Load(string url)
+        {
+            if (!File.Exists(url))
+            {
+                throw new Exception("Cannot find file");
+            }
+
+            using (var file = File.OpenRead(url))
+            {
+                doc.Load(file);
+                parser.Document = doc;
+            }
+        }
+    }
 }

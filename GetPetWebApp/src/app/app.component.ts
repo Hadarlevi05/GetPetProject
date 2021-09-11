5e1b732b03be87b32f6871ed4c436345683ef125
Sedri14 <sedri14@gmail.com>
2021-09-11 12:15:18 +0300

    adding top bar counter, pet-view size change

--- GetPetWebApp/src/app/app.component.ts
+++ GetPetWebApp/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PetFilter } from './modules/pets/models/pet-filter';
+import { PetsService } from './modules/pets/services/pets.service';
+import { PetStatus } from './shared/enums/pet-status';
 import { IUser } from './shared/models/iuser';
 import { AuthenticationService } from './shared/services/authentication.service';
 
@@ -13,18 +16,60 @@ export class AppComponent implements OnInit {
   user: IUser = <IUser>{};
   isLogin = false;
 
+  articleLoading = true;
+  waitingForAdoptionCount: number = 0;
+  waitingForAdoptionAnimation: number = 0;
+  adoptedCount: number = -1;
+
   constructor(
-    private authenticationService: AuthenticationService) { }
+    private authenticationService: AuthenticationService,
+    private petsService: PetsService) { }
 
   ngOnInit() {
     this.authenticationService.currentUser.subscribe(user => {
       this.user = user;
       this.isLogin = user.id > 0;
     });
+
+    this.setCounters();
+  }
+
+  setCounters() {
+
+    const date = new Date();
+    date.setDate(date.getDate() - 1000);
+
+
+    const filter = new PetFilter(1, 100, date, [], undefined, undefined, PetStatus.WaitingForAdoption);
+    this.petsService.searchCount(filter).subscribe(counter => {
+      this.waitingForAdoptionCount = counter.count;
+
+      console.log("WAITING FOR ADOPTION: ", this.waitingForAdoptionCount);
+
+      var waitingForAdoptionStop: any = setInterval(() => {
+        console.log("waitfor count", this.waitingForAdoptionCount);
+        if (this.waitingForAdoptionCount <= 0) {
+          clearInterval(waitingForAdoptionStop);
+        } else {
+          this.waitingForAdoptionAnimation++;
+          console.log("counting:", this.waitingForAdoptionAnimation);
+          if (this.waitingForAdoptionAnimation == this.waitingForAdoptionCount) {
+            clearInterval(waitingForAdoptionStop);
+          }
+        }
+      }, 10);
+
+
+    });
+
+    filter.petStatus = PetStatus.Adopted;
+    this.petsService.searchCount(filter).subscribe(counter => {
+      this.adoptedCount = counter.count;
+    });
   }
 
   logout() {
     this.authenticationService.logout();
-  }
 
+  }
 }
